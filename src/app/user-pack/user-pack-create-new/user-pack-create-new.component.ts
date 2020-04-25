23232dab1d82eb23c7161785ededc528a6bcafe1
udithaabeykoon <udithaedu@gmail.com>
2020-04-25 15:17:03 +0530

    create user pack

--- src/app/user-pack/user-pack-create-new/user-pack-create-new.component.ts
+++ src/app/user-pack/user-pack-create-new/user-pack-create-new.component.ts
@@ -14,82 +14,88 @@ export class UserPackCreateNewComponent implements OnInit {
 
   constructor(private activatedRout:ActivatedRoute,
               private productService:ProductsService ) { }
+//define selecte item variables
+  selectedItemweight = 200;
+  selectedID:string;
+  selectedItemName:string;
+  selectedItemImg:string;
+  selectedItemUnitPrice:number;
+  selectedItemTotalPrice:number = 0;
+  packTotalPrice=0;
+
+  
 
   items:Products[];
-
-  elements: any = [
-    {id: 1,imgview: 'photo',name: 'Carrot', quantity: '100g', price: 'Rs.120/='},
-    {id: 2,imgview: 'photo' ,name: 'Cabbage', quantity: '300g', price: 'Rs.150/='},
-  ];
-
-  weight = 200;
-  totalPrice = 0; 
-
+  addedItems=[];
   headElements =   ['No.','Product View' ,'Product name', 'Quantity', 'Price', 'Remove'];
 
   ngOnInit(): void {
-    console.log(this.activatedRout.params['id']);
-
+  //get all product array from product service
     this.items = this.productService.getProducts();
     
 
   }
+  
 
-  //selected: number;
-  selectedid:string;
-  unitdetails:any;
-  unitvalues;
-  unitprice;
-  unitimg;
-  unitid;
-  unitname;
 
   selectOption(ID: string) {
-    //console.log(ID);
-    this.selectedid = ID
-   // this.unitprice = this.items.find( ({ productID }) => productID === '001' );
-   this.unitdetails = this.items.find(x => x.productID === ID);
-   
-      // for (var x in this.unitdetails){
-      //   this.unitdetails.hasOwnProperty(x) && this.res.push(this.unitdetails[x])
-      // }
-      this.unitvalues  = Object.keys(this.unitdetails).map(it => this.unitdetails
-        [it]);
-        this.unitprice = this.unitvalues[2];
-        this.unitimg = this.unitvalues[6];
-        this.unitid = this.unitvalues[0];
-        this.unitname = this.unitvalues[1];
-        this.weight = 200;
-        this.totalPrice = this.unitprice*2;
-        return '${this.unitprice} ${this.unitimg} ${this.weight} ${this.unitid} ${this.unitname}'
-        //console.log(this.unitvalues)
-        //return this.unitprice = this.unitvalues[2]; 
-      // return this.unitimg = this.unitvalues[3];
+
+  //recieve all the details related to selected item
+    console.log(ID);
+    this.selectedID = ID
+    this.selectedItemName = this.items.find((x)=>x.productID===ID).productName;
+    this.selectedItemImg = this.items.find((x)=>x.productID===ID).imgSrc;
+    this.selectedItemUnitPrice = this.items.find((x)=>x.productID===ID).unitPrice;
+    this.selectedItemTotalPrice = this.selectedItemUnitPrice*(this.selectedItemweight/100);
+
   }
 
 
   onPlusClick(){
-    this.weight=this.weight+100;
-    this.totalPrice = this.unitprice*(this.weight/100);
+  //increase weight
+    this.selectedItemweight=this.selectedItemweight+100;
+  //update total weigt according to icrease weight
+    this.selectedItemTotalPrice = this.selectedItemUnitPrice*(this.selectedItemweight/100);
   }
 
   onMinusClick(){
-    if(this.weight>200){
-    this.weight=this.weight-100;
-    this.totalPrice = this.unitprice*(this.weight/100);
+  
+    if(this.selectedItemweight>200){
+  //decrease weight
+    this.selectedItemweight=this.selectedItemweight-100;
+  //update total weigt according to icrease weight
+    this.selectedItemTotalPrice = this.selectedItemUnitPrice*(this.selectedItemweight/100);
     }
   }
 
-  tabledata;
-  res = [];
+
 
   addToTable(){
-    this.tabledata = [this.unitid,this.unitimg,this.unitname, this.weight, this.unitprice];
-    for (var x in this.tabledata){
-        this.tabledata.hasOwnProperty(x) && this.res.push(this.tabledata[x])
-      }
-    console.log(this.res)
+//add item object to addeditems array
+    this.addedItems.push({productID:this.selectedID,productName:this.selectedItemName,imgSrc:this.selectedItemImg,weight:this.selectedItemweight,totalPricePerItem:this.selectedItemTotalPrice});
+//update total price 
+    this.packTotalPrice = this.calculateTotalPrice(this.addedItems);
   
+  }
+
+  onRemoveClick(productID){
+  //remove some ite from selected list array
+    let selectedObject = this.addedItems.find((x)=>x.productID===productID);
+    let index = this.addedItems.indexOf(selectedObject);
+    this.addedItems.splice(index,1);
+  //update total price after removing some item
+    this.packTotalPrice = this.calculateTotalPrice(this.addedItems);
+  }
+
+
+//calculate total pack price 
+  calculateTotalPrice(addedItems){
+    let total=0;
+    let array = addedItems;
+    for(let x of array){
+      total = total+x.totalPricePerItem;
+    }
+    return total;
   }
 
   
